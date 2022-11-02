f6932e5ceb1806543a55f366d5e0875929e514c3
Dempoleon <dempoleon@gmail.com>
2022-11-01 22:01:22 -0700

    implemented employee role update

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function mainMenu() {
             ]
         },
     ]).then(async (answer) => {
-        switch(answer.main_menu) {
+        switch (answer.main_menu) {
             case 'View all departments':
                 viewDepartments();
                 break;
@@ -55,32 +55,32 @@ function mainMenu() {
                 break;
             case 'Quit':
                 process.exit();
-                
+
         }
 
-    }).then(()=>{
+    }).then(() => {
         mainMenu();
     })
-    
+
 }
 
 
 
 function viewDepartments() {
     db.query('SELECT * FROM departments ORDER BY name', (err, result) => {
-        console.table('\nDepartments',result);
+        console.table('\nDepartments', result);
     });
 }
 
 function viewRoles() {
-    db.query(`SELECT roles.id, roles.title, departments.name AS department, roles.salary FROM roles INNER JOIN departments on roles.department_id = departments.id`, (err, result) => {
-        console.table('\nRoles',result);
+    db.query(`SELECT roles.id, roles.title, departments.name AS department, roles.salary FROM roles INNER JOIN departments on roles.department_id = departments.id ORDER BY roles.id`, (err, result) => {
+        console.table('\nRoles', result);
     })
 }
 
 function viewEmployees() {
     db.query('SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, employees.manager_id as manager FROM employees INNER JOIN roles on employees.role_id = roles.id INNER JOIN departments on roles.department_id = departments.id ORDER BY employees.id;', (err, result) => {
-        console.table('\nEmployees',result);
+        console.table('\nEmployees', result);
     })
 }
 
@@ -89,10 +89,9 @@ async function addDepartment() {
         type: 'input',
         name: 'department',
         message: 'What is the name of the department?'
-    }).then(async(answer) => {
-        console.log(answer.department);
-        db.query(`INSERT INTO departments (name) VALUES (?)`, answer.department, async(err, result) => {
-            console.log(`inserted ? into departments`, answer.department);
+    }).then(async (answer) => {
+        db.query(`INSERT INTO departments (name) VALUES (?)`, answer.department, async (err, result) => {
+            console.log(`inserted ${answer.department} into departments`);
         })
     })
 }
@@ -123,15 +122,15 @@ async function addRole() {
         }
     ]).then(async (answers) => {
         let dep_id;
-        for(let i = 0; i < departmentNames.length; i++) {
-            if(departmentNames[i] === answers.dept) {
-                console.log(departmentNames[i]);
+        for (let i = 0; i < departmentNames.length; i++) {
+            if (departmentNames[i] === answers.dept) {
+
                 dep_id = i + 1;
             }
         }
         db.query(`INSERT INTO roles(title, department_id, salary) VALUES ('${answers.title}',${dep_id},${answers.salary})`, async (err, result) => {
-            console.log('inserted ? into roles', answers.title);
-            console.log(err);
+            console.log(`Inserted ${answers.title} into roles`);
+
         })
     })
 }
@@ -144,7 +143,7 @@ async function addEmployee() {
     roles.forEach(element => {
         roleTitles.push(element.title);
     });
-    console.log(roleTitles);
+
     var managers = await db.promise().query('SELECT * FROM employees ORDER BY employees.id');
     managers = managers[0];
 
@@ -152,7 +151,6 @@ async function addEmployee() {
     managers.forEach(manager => {
         managerNames.push(manager.first_name);
     });
-    console.log(managerNames);
     await inquirer.prompt([
         {
             type: 'input',
@@ -179,30 +177,67 @@ async function addEmployee() {
     ]).then(async (answers) => {
 
         let role_id;
-        for(let i = 0; i < roleTitles.length; i++) {
-            if(roleTitles[i] === answers.role) {
-                console.log(roleTitles[i]);
+        for (let i = 0; i < roleTitles.length; i++) {
+            if (roleTitles[i] === answers.role) {
                 role_id = i + 1;
             }
         }
 
         let manager_id;
-        for(let i = 0; i < managerNames.length; i++) {
-            if(managerNames[i] === answers.manager) {
-                console.log(managerNames[i]);
+        for (let i = 0; i < managerNames.length; i++) {
+            if (managerNames[i] === answers.manager) {
                 manager_id = i + 1;
             }
         }
 
         db.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES ('${answers.first_name}','${answers.last_name}',${role_id}, ${manager_id})`, async (err, result) => {
-            console.log('inserted ? into roles', answers.title);
-            console.log(err);
+            console.log('inserted ? ? into employees', [answers.first_name, answers.last_name]);
+
         })
     })
 }
 
 async function updateEmployee() {
-    
+    var employeeList = await db.promise().query(`SELECT *, CONCAT(first_name,' ', last_name) as name FROM employees ORDER BY id`);
+    employeeList = employeeList[0];
+
+    var employeeArr = [];
+    employeeList.forEach(employee => {
+        employeeArr.push(employee.name)
+    })
+
+
+    var roles = await db.promise().query('SELECT * FROM roles ORDER BY roles.id');
+    roles = roles[0];
+
+    var roleTitles = [];
+    roles.forEach(element => {
+        roleTitles.push(element.title);
+    });
+
+
+    await inquirer.prompt(
+        [{
+            type: 'list',
+            name: 'employee',
+            message: `Which employee's role do you want to update?`,
+            choices: employeeArr
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: `Which role do you want to assign to the selected employee?`,
+            choices: roleTitles
+        }]
+    ).then(async (answer) => {
+        var employee_id = employeeArr.indexOf(answer.employee) + 1;
+
+        var role_id = roleTitles.indexOf(answer.role) + 1;
+        db.query(`UPDATE employees SET employees.role_id = ${role_id} WHERE employees.id = ${employee_id}`, async (err, result) => {
+            console.log(`Updated ${answer.employee} role to ${answer.role}`);
+            console.log(err);
+        })
+    })
 }
 
 
